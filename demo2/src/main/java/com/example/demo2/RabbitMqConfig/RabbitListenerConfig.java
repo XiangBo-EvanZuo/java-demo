475778484c375fe8e50b440b6bc30ff6130bb8a1
Evan Zuo <739221432@qq.com>
2023-03-14 11:37:44 +0800

    feat: log

--- demo2/src/main/java/com/example/demo2/RabbitMqConfig/RabbitListenerConfig.java
+++ demo2/src/main/java/com/example/demo2/RabbitMqConfig/RabbitListenerConfig.java
@@ -1,13 +1,17 @@
 package com.example.demo2.RabbitMqConfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class RabbitListenerConfig {
 
     @RabbitListener(queues = "hello")
     public void rabbitListener(String msg) {
+
         System.out.println("listen " + msg);
+        log.info(msg);
     }
 }
